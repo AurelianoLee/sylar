38117f43f8193152d8f7127a5d447d81ddc42dd8
aure-lee <1425153862@qq.com>
2025-09-26 17:51:59 +0800

    learn: cmakelists.txt

--- sylar/util.cc
+++ sylar/util.cc
@@ -2,7 +2,13 @@
 
 namespace sylar {
 pid_t GetThreadId() {
+#ifdef __APPLE__
+    uint64_t tid;
+    pthread_threadid_np(NULL, &tid);
+    return tid;
+#elif __linux__
     return syscall(SYS_gettid);
+#endif
 }
 
 uint32_t GetFiberId() {
